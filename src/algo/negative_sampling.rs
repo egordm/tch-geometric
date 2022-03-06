3fb9eda4129b3a37eb287263be077c8fbe86c4c2
Egor Dmitriev <egordmitriev2@gmail.com>
2022-03-06 22:05:59 +0100

    feat: Updated negative sampling functions to yield subgraphs instead of edgelists

--- src/algo/negative_sampling.rs
+++ src/algo/negative_sampling.rs
@@ -1,69 +1,116 @@
 use std::collections::HashMap;
 use rand::{Rng};
-use tch::{ Tensor};
+use tch::{Tensor};
 use rayon::prelude::*;
 use crate::data::{CooGraphBuilder, CsrGraph};
-use crate::utils::{NodeIdx, NodeType, random, RelType, TensorResult, try_tensor_to_slice, try_tensor_to_slice_mut};
+use crate::utils::{EdgeType, NodeIdx, NodePtr, NodeType, random, RelType, TensorResult, try_tensor_to_slice, try_tensor_to_slice_mut};
 
 pub fn negative_sample_neighbors(
+    rng: &mut impl Rng,
     graph: &CsrGraph,
     graph_size: (i64, i64),
-    inputs: &Tensor,
+    inputs: &[NodeIdx],
     num_neg: i64,
     try_count: i64,
-) -> TensorResult<(Tensor, Vec<usize>)> {
+) -> (
+    Vec<NodeIdx>,
+    CooGraphBuilder,
+    usize,
+) {
+
+    // Initialize some data structures for the sampling process
+    let mut samples: Vec<NodeIdx> = Vec::new();
+    let mut samples_mapping: HashMap<NodeIdx, NodePtr<usize>> = HashMap::new();
+
+    let mut edge_index = CooGraphBuilder::default();
+
+    samples.extend_from_slice(inputs);
+    samples_mapping.extend(samples.iter().enumerate().map(|(i, &s)| (s, i)));
+
     let node_count = graph_size.1;
-    let v_data = inputs.repeat_interleave_self_int(num_neg, None, None);
-    let mut w_data = v_data.zeros_like();
+    let sample_count = samples.len();
 
-    let v = try_tensor_to_slice::<i64>(&v_data)?;
-    let w = try_tensor_to_slice_mut::<i64>(&mut w_data)?;
+    for i in 0..samples.len() as i64 {
+        let v = samples[i as usize];
 
-    let mask: Vec<_> = w.into_par_iter().enumerate().map_init(
-        random::rng_get,
-        |rng, (i, w)| {
+        for _ in 0..num_neg {
             for _t in 0..try_count {
-                *w = rng.gen_range(0..node_count);
-                if !graph.has_edge(v[i], *w) {
-                    return None;
+                let w = rng.gen_range(0..node_count);
+                if !graph.has_edge(v, w) {
+                    let j = *samples_mapping.entry(v).or_insert(samples.len());
+                    edge_index.push_edge(i as i64, j as i64, -1);
+                    break;
                 }
             }
-
-            Some(i)
         }
-    ).filter_map(|v| v).collect();
+    }
 
-    let output = Tensor::vstack(&[v_data, w_data]);
-    Ok((output, mask))
+    (samples, edge_index, sample_count)
 }
-
 
 pub fn negative_sample_neighbors_heterogenous(
     rng: &mut impl Rng,
+    node_types: &[NodeType],
+    edge_types: &[EdgeType],
     graphs: &HashMap<RelType, (CsrGraph, (i64, i64))>,
-    node_rels: &HashMap<NodeType, Vec<RelType>>,
     inputs: &HashMap<NodeType, &[NodeIdx]>,
     num_neg: i64,
     try_count: i64,
-) -> TensorResult<HashMap<RelType, CooGraphBuilder>> {
+) -> (
+    HashMap<NodeType, Vec<NodeIdx>>,
+    HashMap<RelType, CooGraphBuilder>,
+    HashMap<NodeType, usize>,
+) {
+    // Create node to reltype mapping
+    let mut node_rels: HashMap<NodeType, Vec<(RelType, NodeType)>> = HashMap::default();
+    for (src, rel, dst) in edge_types {
+        let rel_type = format!("{}__{}__{}", src, rel, dst);
+        node_rels.entry(src.clone())
+            .or_insert_with(Vec::new)
+            .push((rel_type, dst.clone()));
+    }
 
-    let mut result: HashMap<RelType, CooGraphBuilder> = graphs.keys()
-        .map(|k| (k.clone(), CooGraphBuilder::new()))
+    // Initialize some data structures for the sampling process
+    let mut samples: HashMap<NodeType, Vec<NodeIdx>> = HashMap::new();
+    let mut samples_mapping: HashMap<NodeType, HashMap<NodeIdx, NodePtr<usize>>> = HashMap::new();
+
+    for node_type in node_types {
+        let samples = samples
+            .entry(node_type.clone())
+            .or_insert_with(Vec::new);
+        if let Some(inputs) = inputs.get(node_type) {
+            samples.extend_from_slice(inputs);
+        }
+
+        let samples_mapping = samples_mapping
+            .entry(node_type.clone())
+            .or_insert_with(HashMap::new);
+        if inputs.contains_key(node_type) {
+            samples_mapping.extend(samples.iter().enumerate().map(|(i, &s)| (s, i)));
+        }
+    }
+
+    let mut edge_index: HashMap<RelType, CooGraphBuilder> = graphs.keys()
+        .map(|rel_type| (rel_type.clone(), CooGraphBuilder::new()))
         .collect();
+    let sample_count: HashMap<NodeType, usize> = samples.iter().map(|(t, v)| (t.clone(), v.len())).collect();
 
-    for (node_type, inputs) in inputs{
+    // Maintains begin/end indices for each node type
+    for (node_type, inputs) in inputs {
         let node_rels = &node_rels[node_type];
 
-        for v in inputs.iter().cloned() {
+        for (i, &v) in inputs.iter().enumerate() {
             for _ in 0..num_neg {
-                let rel_type = &node_rels[rng.gen_range(0..node_rels.len())];
-                let (graph, size) = &graphs[rel_type];
-                let result = result.get_mut(rel_type).unwrap();
+                let (rel_type, dst_type) = &node_rels[rng.gen_range(0..node_rels.len())];
+                let (graph, (_, node_count)) = &graphs[rel_type];
+                let samples_mapping = samples_mapping.get_mut(dst_type).unwrap();
+                let edge_index = edge_index.get_mut(rel_type).unwrap();
 
                 for _t in 0..try_count {
-                    let w = rng.gen_range(0..size.1);
+                    let w = rng.gen_range(0..*node_count);
                     if !graph.has_edge(v, w) {
-                        result.push_edge(v, w, -1);
+                        let j = *samples_mapping.entry(v).or_insert(samples.len());
+                        edge_index.push_edge(i as i64, j as i64, -1);
                         break;
                     }
                 }
@@ -71,7 +118,7 @@ pub fn negative_sample_neighbors_heterogenous(
         }
     }
 
-    Ok(result)
+    (samples, edge_index, sample_count)
 }
 
 // TODO: benchmark current impl vs per thread retry
@@ -91,30 +138,26 @@ mod tests {
     pub fn test_negative_sample_neighbors() {
         let (x, _, coo_graph) = load_karate_graph();
 
+        let mut rng = rand::rngs::SmallRng::from_seed([0; 32]);
+
         let node_count = x.size()[0];
         let graph_data = CsrGraphStorage::try_from(&coo_graph).unwrap();
         let graph = CsrGraph::<i64, i64>::try_from(&graph_data).unwrap();
 
-        let test: Vec<_> = (0..node_count).collect();
-        let inputs = Tensor::of_slice(&test);
-        let (output, mask) = super::negative_sample_neighbors(
+        let inputs: Vec<_> = (0..node_count).collect();
+
+        let (samples, edge_index, _sample_count) = super::negative_sample_neighbors(
+            &mut rng,
             &graph,
             (node_count, node_count),
             &inputs,
             10,
             5,
-        ).unwrap();
-        let output_size = output.size()[1] as usize;
+        );
 
-        let data = try_tensor_to_slice::<i64>(&output).unwrap();
-        let v = &data[0..output_size];
-        let w = &data[output_size..];
-
-        let _error_count = mask.len();
-        for (i, (v, w)) in v.iter().zip(w.iter()).enumerate() {
-            if !mask.contains(&i) {
-                assert!(!graph.has_edge(*v, *w));
-            }
+        for (&i, &j) in edge_index.rows.iter().zip(edge_index.cols.iter()) {
+            let (v, w) = (samples[i as usize], samples[j as usize]);
+            assert!(!graph.has_edge(v, w));
         }
     }
 
@@ -157,23 +200,25 @@ mod tests {
             node_rels.entry(src.clone()).or_insert(vec![]).push(rel_type.clone());
         }
 
-        let result = negative_sample_neighbors_heterogenous(
+        let (samples, edge_index, _sample_count) = negative_sample_neighbors_heterogenous(
             &mut rng,
+            &node_types,
+            &edge_types,
             &graphs,
-            &node_rels,
             &inputs,
             3,
             10,
-        ).unwrap();
+        );
 
-        for (rel_type, coo_builder) in result {
+        for (rel_type, edge_index) in edge_index {
             let (graph, _) = &graphs[&rel_type];
+            let (src, _, dst) = &to_edge_types[&rel_type];
+            let (src_samples, dst_samples) = (&samples[src], &samples[dst]);
 
-            for i in coo_builder.rows.iter().zip(coo_builder.cols.iter()) {
-                let (v, w) = i;
-                assert!(!graph.has_edge(*v, *w));
+            for (&i, &j) in edge_index.rows.iter().zip(edge_index.cols.iter()) {
+                let (v, w) = (src_samples[i as usize], dst_samples[j as usize]);
+                assert!(!graph.has_edge(v, w));
             }
         }
-
     }
 }
