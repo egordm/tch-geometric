3851c44c912aaa5d3c4e56aa875ae7fe203f79ec
Egor Dmitriev <egordmitriev2@gmail.com>
2022-03-08 23:50:37 +0100

    refactor: Changed negative sampling to return a subgraph

--- tch_geometric/transforms/negative_sampling.py
+++ tch_geometric/transforms/negative_sampling.py
@@ -6,7 +6,9 @@ from torch_geometric.data import Data, HeteroData
 
 import tch_geometric.tch_geometric as native
 from tch_geometric.data import to_csc, to_csr, to_hetero_csc, to_hetero_csr
+from tch_geometric.data.subgraph import build_subgraph
 from tch_geometric.types import MixedData, validate_mixeddata
+from tch_geometric.utils import zip_dict
 
 
 class NegativeSamplerTransform:
@@ -36,10 +38,11 @@ class NegativeSamplerTransform:
         else:
             raise TypeError(f'Invalid graph type: {type(data)}')
 
-    def __call__(self, inputs: MixedData) -> Tuple[Tensor, Tuple[Tensor, Tensor], int]:
+    def __call__(self, inputs: MixedData) -> HeteroData:
         if isinstance(self.data, Data):
             validate_mixeddata(inputs, hetero=False, dtype=torch.int64)
 
+            # Sample negative edges.
             sample_fn = native.negative_sample_neighbors_homogenous
             node, row, col, sample_count = sample_fn(
                 self.ptrs,
@@ -50,11 +53,18 @@ class NegativeSamplerTransform:
                 self.try_count,
             )
 
-            return node, (row, col), sample_count
+            # Build the subgraph
+            subgraph = Data()
+            subgraph.x = node
+            subgraph.edge_index = torch.stack([row, col], dim=0)
+            subgraph.sample_count = sample_count
+
+            return subgraph
 
         elif isinstance(self.data, HeteroData):
             validate_mixeddata(inputs, hetero=True, dtype=torch.int64)
 
+            # Sample negative edges.
             sample_fn = native.negative_sample_neighbors_heterogenous
             nodes, rows, cols, sample_counts = sample_fn(
                 self.node_types,
@@ -67,4 +77,7 @@ class NegativeSamplerTransform:
                 self.try_count,
             )
 
-            return nodes, (rows, cols), sample_counts
+            subgraph = build_subgraph(nodes, rows, cols, node_attrs=dict(sample_count=sample_counts))
+
+            return subgraph
+            # return nodes, (rows, cols), sample_counts
