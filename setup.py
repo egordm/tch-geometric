c2d4bf2420b7b264f01d7ea7aa30ced79f42aa3a
Egor Dmitriev <egordmitriev2@gmail.com>
2022-05-21 23:10:56 +0200

    feat: Added package uploading and versioning

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ import os
 
 from setuptools import setup
 from setuptools_rust import RustExtension
+import toml
+
+version = toml.load('Cargo.toml')['package']['version']
+if os.getenv('VERSION_SUFFIX', False):
+    version = f'{version}+{os.getenv("VERSION_SUFFIX")}'
 
 setup(
     rust_extensions=[
@@ -10,4 +15,5 @@ setup(
             debug=os.environ.get("BUILD_DEBUG") == "1",
         )
     ],
+    version=version
 )
