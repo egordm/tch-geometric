c0cc4c38810488af7ffdb955bbd890f37fa75807
Egor Dmitriev <egordmitriev2@gmail.com>
2022-05-22 14:24:30 +0200

    feat: RIp maturin

--- conda/tch_geometric/build_conda.sh
+++ conda/tch_geometric/build_conda.sh
@@ -4,16 +4,12 @@ export PYTHON_VERSION=$1
 export TORCH_VERSION=$2
 export CUDA_VERSION=$3
 
-echo Setup Cuda Args
-export TORCH_CUDA_VERSION=$(echo $CUDA_VERSION | sed "s/cuda/cu/g")
-echo "TORCH_CUDA_VERSION=$TORCH_CUDA_VERSION" >> $GITHUB_ENV
-
 export CONDA_PYTORCH_CONSTRAINT="pytorch==${TORCH_VERSION%.*}.*"
 
-if [ "${TORCH_CUDA_VERSION}" = "cpu" ]; then
+if [ "${CUDA_VERSION}" = "cpu" ]; then
   export CONDA_CUDATOOLKIT_CONSTRAINT="cpuonly  # [not osx]"
 else
-  case $TORCH_CUDA_VERSION in
+  case $CUDA_VERSION in
     cu115)
       export CONDA_CUDATOOLKIT_CONSTRAINT="cudatoolkit==11.5.*"
       ;;
@@ -30,13 +26,13 @@ else
       export CONDA_CUDATOOLKIT_CONSTRAINT="cudatoolkit==10.1.*"
       ;;
     *)
-      echo "Unrecognized CUDA_VERSION=$TORCH_CUDA_VERSION"
+      echo "Unrecognized CUDA_VERSION=$CUDA_VERSION"
       exit 1
       ;;
   esac
 fi
 
-echo "PyTorch $TORCH_VERSION+$TORCH_CUDA_VERSION"
+echo "PyTorch $TORCH_VERSION+$CUDA_VERSION"
 echo "- $CONDA_PYTORCH_CONSTRAINT"
 echo "- $CONDA_CUDATOOLKIT_CONSTRAINT"
 
