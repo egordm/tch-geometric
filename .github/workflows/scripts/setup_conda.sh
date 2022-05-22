689e50fd67024136908858b1f923bf2415c46078
Egor Dmitriev <egordmitriev2@gmail.com>
2022-05-22 13:09:49 +0200

    feat: RIp maturin

--- .github/workflows/scripts/setup_conda.sh
+++ .github/workflows/scripts/setup_conda.sh
@@ -2,3 +2,7 @@
 
 echo Install Conda Build
 conda install conda-build conda-verify --yes
+
+echo Setup Cuda Args
+export TORCH_CUDA_VERSION=$(echo $CUDA_VERSION | sed "s/cuda/cu/g")
+echo "TORCH_CUDA_VERSION=$TORCH_CUDA_VERSION" >> $GITHUB_ENV
