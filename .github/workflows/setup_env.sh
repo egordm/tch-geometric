50c02dd4d92ed91d9d8bb172d2dc74b874c2bc63
Egor Dmitriev <egordmitriev2@gmail.com>
2022-05-22 02:58:16 +0200

    feat: back to maturin

--- .github/workflows/setup_env.sh
+++ .github/workflows/setup_env.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo Setup Python
+export PYTHON_ALIAS=$(
+  echo '{ "3.7": "cp37-cp37m", "3.8": "cp38-cp38", "3.9": "cp39-cp39", "3.10": "cp310-cp310" }' \
+    | jq -r 'to_entries[] | select(.key=="3.9") | .value'
+)
 export PATH=/opt/python/$PYTHON_ALIAS/bin/:$PATH
 export PYTHON_INCLUDE_DIRS=$(python -c "from sysconfig import get_paths as gp; print(gp()['include'])")
 export PYTHON_SITE_DIR=$(python -c "import site; print(site.getsitepackages()[0])")
@@ -10,6 +14,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH=$HOME/.cargo/bin:$PATH
 
 echo Install PyTorch $TORCH_VERSION+$CUDA_VERSION
+export TORCH_CUDA_VERSION=$(echo $CUDA_VERSION | sed "s/cuda/cu/g")
 pip install numpy typing-extensions dataclasses
 pip install --no-index --no-cache-dir torch==$TORCH_VERSION -f https://download.pytorch.org/whl/$CUDA_VERSION/torch_stable.html
 python -c "import torch; print('PyTorch:', torch.__version__)"
@@ -19,3 +24,4 @@ echo "PATH=$PATH" >> $GITHUB_ENV
 echo "PYTHON_INCLUDE_DIRS=$PYTHON_INCLUDE_DIRS" >> $GITHUB_ENV
 echo "LIBTORCH=$PYTHON_SITE_DIR/torch" >> $GITHUB_ENV
 echo "LD_LIBRARY_PATH=$PYTHON_SITE_DIR/torch/lib:$LD_LIBRARY_PATH" >> $GITHUB_ENV
+echo "TORCH_CUDA_VERSION=$TORCH_CUDA_VERSION" >> $GITHUB_ENV
