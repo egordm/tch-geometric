34b6d485bf10b8a6b3d27e2bd27c2b2f658914a3
Egor Dmitriev <egordmitriev2@gmail.com>
2022-05-22 14:21:14 +0200

    feat: RIp maturin

--- .github/workflows/scripts/setup_conda.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Install Conda Build
-#micromamba install conda-build conda-verify --yes
-#micromamba install boa -c conda-forge
-
-echo Setup Cuda Args
-export TORCH_CUDA_VERSION=$(echo $CUDA_VERSION | sed "s/cuda/cu/g")
-echo "TORCH_CUDA_VERSION=$TORCH_CUDA_VERSION" >> $GITHUB_ENV
